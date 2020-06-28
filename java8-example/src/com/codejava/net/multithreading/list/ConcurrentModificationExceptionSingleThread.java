c46ec252a7b70aac9d6f8c0296426c70e1e3342f
rabi0 <rabi0@LAPTOP-3PARCRTL>
2020-06-28 09:40:12 +0530

    Single Thread - itr.remove() instead of lst.remove(3)

---
+++ java8-example/src/com/codejava/net/multithreading/list/ConcurrentModificationExceptionSingleThread.java
@@ -0,0 +1,48 @@
+package com.codejava.net.multithreading.list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * https://techvidvan.com/tutorials/java-concurrentmodificationexception/
+ * 
+ * @author rabi0
+ *
+ */
+public class ConcurrentModificationExceptionSingleThread {
+	public static void main(String[] args) {
+		/**
+		 * Case#1 ConcurrentModificationException using ArrayList
+		 */
+		ArrayList<Integer> list = new ArrayList<>();
+
+		/**
+		 * Case#2 Fix of Case#1 ++++ use of CopyOnWriteArrayList
+		 */
+		// CopyOnWriteArrayList<Integer> list = new CopyOnWriteArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.forEach(System.out::println);
+
+		Iterator<Integer> itr = list.iterator();
+		while (itr.hasNext()) {
+			Integer value = itr.next();
+			if (value.equals(3)) {// 3 will be removed
+				list.remove(value); /// It will throw ConcurrentModificationException!!!
+				// itr.remove(); //Fix
+			}
+		}
+		System.out.println("*****Modified  List and 3 removed!!!**********");
+		list.forEach(System.out::println);
+
+//		
+//		
+//		list.stream().filter(s -> !(s == 3)).forEach(System.out::println);
+
+
+	}
+
+}
