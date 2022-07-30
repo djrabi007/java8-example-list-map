d804a5aaf2935539b9a6930578e67a49960dadea
djrabi007 <rabi007007@gmail.com>
2022-07-30 20:06:33 +0530

    Add files via upload
    java8 Stream Notun Surjjo!!!

---
+++ java8-example/src/com/dailycodebuffer/java8/Java8Utility.java
@@ -0,0 +1,118 @@
+package com.dailycodebuffer.java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Java8Utility {
+
+	static List<Employee> employees = new ArrayList<>();
+	static List<String> lstOfProject1 = new ArrayList<>();
+	static List<String> lstOfProject2 = new ArrayList<>();
+	static List<String> lstOfProject3 = new ArrayList<>();
+
+	static {
+		lstOfProject1.add("Project 1");
+		lstOfProject1.add("Project 2");
+		lstOfProject2.add("Project 1");
+		lstOfProject2.add("Project 3");
+		lstOfProject3.add("Project 3");
+		lstOfProject3.add("Project 4");
+
+		employees.add(new Employee("Shabbir", "Dawoodi", 5000.0, lstOfProject1));
+
+		employees.add(new Employee("Nikhil", "Gupta", 6000.0, lstOfProject2));
+
+		employees.add(new Employee("Shivam", "Kumar", 5500.0, lstOfProject3));
+
+	}
+
+	public static void shortCircuitExampleV2() {
+		Stream.generate(Math::random).limit(5).forEach(value -> System.out.println(value));
+	}
+
+	public static void reduceExample() {
+		Double totalSal = employees.stream().map(employee -> employee.getSalary()).reduce(0.0, Double::sum);
+		System.out.println(totalSal);
+	}
+
+	public static void minMaxExample() {
+		employees.stream().max(Comparator.comparing(Employee::getSalary)).orElseThrow(NoSuchElementException::new);
+	}
+
+	public static void sortingExample() {
+		List<Employee> sortedEmployees = employees.stream()
+				.sorted((o1, o2) -> o1.getFirstName().compareToIgnoreCase(o2.getFirstName()))
+				.collect(Collectors.toList());
+		System.out.println(sortedEmployees);
+	}
+
+	public static void shortCircuitExample() {
+		List<Employee> shortCircuit = employees.stream().skip(1).limit(1).collect(Collectors.toList());
+		System.out.println(shortCircuit);
+	}
+
+	public static void flatMapExample() {
+		String projects = employees.stream().map(employee -> employee.getProjects()) // return Stream<List<String>>
+				.flatMap(strings -> strings.stream()) // return Stream<String>
+				.collect(Collectors.joining(","));
+		System.out.println(projects); // Stream of List of Projects not Employee object. ..it will retrun String (Not
+										// Employee)
+	}
+
+	public static void filterAndFindFirstNull() {
+		Employee firstEmployee2 = employees.stream().filter(employee -> employee.getSalary() > 7000.0) //
+				.map(employee -> new Employee(employee.getFirstName(), employee.getLastName(),
+						employee.getSalary() * 1.10, employee.getProjects()))
+				.findFirst() // No Employee have >7k .... so, it will return null
+				.orElse(null);
+		System.out.println(firstEmployee2); // findFirst is the terminal operation ...
+	}
+
+	public static void filterAndFindFirst() {
+		Employee firstEmployee = employees.stream().filter(employee -> employee.getSalary() > 5000.0) //
+				.map(employee -> new Employee(employee.getFirstName(), employee.getLastName(),
+						employee.getSalary() * 1.10, employee.getProjects()))
+				.findFirst() // 1st employee whose salary is >5k .... retruning Employee object only (Not
+								// List)
+				.orElse(null);
+		System.out.println(firstEmployee); // findFirst is the terminal operation ...
+	}
+
+	// filter
+	public static void filterExample() {
+		List<Employee> filterEmployee = employees.stream().filter(employee -> employee.getSalary() > 5000.0) // filter
+																												// is
+																												// taking
+																												// predicate
+																												// i.e.
+																												// if/else
+																												// ...
+				.map(employee -> new Employee(employee.getFirstName(), employee.getLastName(),
+						employee.getSalary() * 1.10, employee.getProjects()))
+				.collect(Collectors.toList());
+
+		System.out.println(filterEmployee); // Filter and map are both intermediate operation ..(. Any operation i.e
+											// returning Stream itself)
+	}
+
+	// map
+	// collect
+	public static void mapExample() {
+		Set<Employee> increasedSal = employees.stream().map(employee -> new Employee(employee.getFirstName(),
+				employee.getLastName(), employee.getSalary() * 1.10, employee.getProjects()))
+				.collect(Collectors.toSet());
+		System.out.println(increasedSal); // Increasing Salary of each employee by 10%
+	}
+
+	public static void forEachExample() {
+		employees.stream().forEach(employee -> System.out.println(employee)); // Terminal operation ...You can not do
+																				// any
+																				// operation after that
+	}
+
+}
---
+++ java8-example/src/com/dailycodebuffer/java8/StreamApiTutorialApplication.java
@@ -0,0 +1,46 @@
+package com.dailycodebuffer.java8;
+
+public class StreamApiTutorialApplication {
+
+
+    public static void main(String[] args) {
+
+        //foreach
+		Java8Utility.forEachExample();
+
+		// map & collect
+		Java8Utility.mapExample();
+
+		// filter
+		Java8Utility.filterExample();
+
+		// FilterAndFindFirst
+		Java8Utility.filterAndFindFirst();
+
+		// Null or Empty value
+		Java8Utility.filterAndFindFirstNull();
+
+		// flatMap
+		Java8Utility.flatMapExample();
+
+		// short Circuit operations
+		Java8Utility.shortCircuitExample();
+
+		// Finite Data
+		Java8Utility.shortCircuitExampleV2();
+
+		// sorting
+		Java8Utility.sortingExample();
+
+		// min or max
+		Java8Utility.minMaxExample();
+
+		// reduce
+		Java8Utility.reduceExample();
+
+    }
+
+}
+
+
+
